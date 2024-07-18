92e28407c30d73c0f5086de197caa39d3cbcbe39
The-G-Man-Half-Life <mateomoospina@gmail.com>
2024-07-18 13:57:38 -0500

    feat: The library was created

---
+++ Models/Library.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Models;
+
+public class Library
+{
+    private List<Book> Books;
+
+
+    
+}
